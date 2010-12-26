eceae775a9d23c79196f3471dc9770754f1260f9
Tim Lovorn <tflovorn@crimson.ua.edu>
2010-12-26 15:14:25 -0600

    initial commit, can connect to db and pull data

---
+++ test_DBConnect.java
@@ -0,0 +1,28 @@
+import java.sql.*;
+
+class test_DBConnect {
+    public static void main(String[] args) {
+        try {
+            doQuery();
+        }
+        catch (SQLException e) {
+            System.out.println("SQL exception occured!");
+            System.out.println(e.toString());
+        }
+    }
+
+    private static void doQuery() throws SQLException {
+        String url = "jdbc:postgresql://localhost/evedb";
+        String props = "?user=postgres&password=postgrespass";
+        Connection conn = DriverManager.getConnection(url + props);
+        Statement st = conn.createStatement();
+        String query = "SELECT * FROM invtypes WHERE typeid=34";
+        ResultSet rs = st.executeQuery(query);
+        while (rs.next()) {
+            System.out.println(rs.getString(1));
+        }
+        rs.close();
+        st.close();
+        conn.close();
+    }
+}
